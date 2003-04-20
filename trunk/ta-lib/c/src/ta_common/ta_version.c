3e54e662a1050d11ae55b4d0c65e3ccc9bc89ad1
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2003-04-20 05:02:36 +0000

    Now V0.0.8

--- trunk/ta-lib/c/src/ta_common/ta_version.c
+++ trunk/ta-lib/c/src/ta_common/ta_version.c
@@ -4,9 +4,9 @@
  */
 #define MAJOR 0
 #define MINOR 0
-#define BUILD 7
+#define BUILD 8
 
-#define VERSION_STRING "0.0.7"
+#define VERSION_STRING "0.0.8"
 
 /* Nothing to modify below this line. */
 
