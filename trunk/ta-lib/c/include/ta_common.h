b404bfeeb9e5fea292a056da4f8bfa6b9a6c6c21
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2003-06-13 01:59:47 +0000

    Update copyright information

--- trunk/ta-lib/c/include/ta_common.h
+++ trunk/ta-lib/c/include/ta_common.h
@@ -1,5 +1,5 @@
 
-/* TA-LIB Copyright (c) 1999-2000, Mario Fortier
+/* TA-LIB Copyright (c) 1999-2003, Mario Fortier
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or
