7b29a5c666fdb87720f1c2d33c83304b8a0ed4c9
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2004-07-03 21:38:10 +0000

    Add test of calling all TA functions with defaults parameters. Add also test of inputs being all zero and some other limit case timeseries.

--- src/tools/ta_regtest/test_abstract.c
+++ src/tools/ta_regtest/test_abstract.c
@@ -44,6 +44,7 @@
  *  -------------------------------------------------------------------
  *  112703 MF   First version.
  *  030104 MF   Add tests for TA_GetLookback
+ *  062504 MF   Add test_default_calls.
  */
 
 /* Description:
@@ -58,7 +59,7 @@
 #include <ctype.h>
 #include "ta_test_priv.h"
 #include "ta_trace.h"
-
+#include "trionan.h"
 
 /**** External functions declarations. ****/
 /* None */
@@ -74,9 +75,26 @@
 
 /**** Local functions declarations.    ****/
 static ErrorNumber testLookback(TA_ParamHolder *paramHolder );
+static ErrorNumber test_default_calls(void);
+static ErrorNumber callWithDefaults( const char *funcName,
+									 const double *input,
+									 const int *input_int, int size );
 
 /**** Local variables definitions.     ****/
-/* None */
+static double inputNegData[100];
+static double inputZeroData[100];
+static double inputRandFltEpsilon[100];
+static double inputRandDblEpsilon[100];
+static double inputRandomData[2000];
+
+static int    inputNegData_int[100];
+static int    inputZeroData_int[100];
+static int    inputRandFltEpsilon_int[100];
+static int    inputRandDblEpsilon_int[100];
+static int    inputRandomData_int[2000];
+
+static double output[10][2000];
+static int    output_int[10][2000];
 
 /**** Global functions definitions.   ****/
 ErrorNumber test_abstract( void )
@@ -127,6 +145,22 @@ ErrorNumber test_abstract( void )
    if( retValue != TA_TEST_PASS )
       return retValue;
 
+   /* Call all the TA functions through the abstract interface. */
+   retValue = allocLib( &udb );
+   if( retValue != TA_TEST_PASS )
+      return retValue;
+
+   retValue = test_default_calls();
+   if( retValue != TA_TEST_PASS )
+   {
+      printf( "TA-Abstract default call failed\n" );
+      return retValue;
+   }
+
+   retValue = freeLib( udb );
+   if( retValue != TA_TEST_PASS )
+      return retValue;
+   
    return TA_TEST_PASS; /* Succcess. */
 }
 
@@ -210,3 +244,173 @@ static ErrorNumber testLookback( TA_ParamHolder *paramHolder )
 }
 
 
+static void testDefault( const TA_FuncInfo *funcInfo, void *opaqueData )
+{
+   ErrorNumber *errorNumber;
+   errorNumber = (ErrorNumber *)opaqueData;
+   if( *errorNumber != TA_TEST_PASS )
+      return;
+
+#define CALL(x) { \
+	*errorNumber = callWithDefaults( funcInfo->name, x, x##_int, sizeof(x)/sizeof(double) ); \
+	if( *errorNumber != TA_TEST_PASS ) { \
+	   printf( "Failed for [%s][%s]\n", funcInfo->name, #x ); \
+       return; \
+	} \
+}
+
+   CALL( inputZeroData );
+   CALL( inputNegData );
+   CALL( inputRandomData );
+   CALL( inputRandFltEpsilon );
+   CALL( inputRandDblEpsilon );
+
+#undef CALL
+}
+
+static ErrorNumber callWithDefaults( const char *funcName, const double *input, const int *input_int, int size )
+{
+   TA_ParamHolder *paramHolder;
+   const TA_FuncHandle *handle;
+   const TA_FuncInfo *funcInfo;
+   const TA_InputParameterInfo *inputInfo;
+   const TA_OutputParameterInfo *outputInfo;
+
+   TA_RetCode retCode;
+   unsigned int i;
+   int j;
+   int outBegIdx, outNbElement;
+
+   retCode = TA_GetFuncHandle( funcName, &handle );
+   if( retCode != TA_SUCCESS )
+   {
+      printf( "Can't get the function handle [%d]\n", retCode );
+      return TA_ABS_TST_FAIL_GETFUNCHANDLE;   
+   }
+                             
+   retCode = TA_ParamHolderAlloc( handle, &paramHolder );
+   if( retCode != TA_SUCCESS )
+   {
+      printf( "Can't allocate the param holder [%d]\n", retCode );
+      return TA_ABS_TST_FAIL_PARAMHOLDERALLOC;
+   }
+
+   TA_GetFuncInfo( handle, &funcInfo );
+
+   for( i=0; i < funcInfo->nbInput; i++ )
+   {
+      TA_GetInputParameterInfo( handle, i, &inputInfo );
+	  switch(inputInfo->type)
+	  {
+	  case TA_Input_Price:
+         TA_SetInputParamPricePtr( paramHolder, i, NULL,
+			 inputInfo->flags&TA_IN_PRICE_OPEN?input:NULL,
+			 inputInfo->flags&TA_IN_PRICE_HIGH?input:NULL,
+			 inputInfo->flags&TA_IN_PRICE_LOW?input:NULL,
+			 inputInfo->flags&TA_IN_PRICE_CLOSE?input:NULL,
+			 inputInfo->flags&TA_IN_PRICE_VOLUME?input_int:NULL, NULL );
+		 break;
+	  case TA_Input_Real:
+         TA_SetInputParamRealPtr( paramHolder, i, input );
+		 break;
+	  case TA_Input_Integer:
+         TA_SetInputParamIntegerPtr( paramHolder, i, input_int );
+         break;
+	  }
+   }
+
+   for( i=0; i < funcInfo->nbOutput; i++ )
+   {
+      TA_GetOutputParameterInfo( handle, i, &outputInfo );
+	  switch(outputInfo->type)
+	  {
+	  case TA_Output_Real:
+	     TA_SetOutputParamRealPtr(paramHolder,i,&output[i][0]);
+		 break;
+	  case TA_Output_Integer:
+	     TA_SetOutputParamIntegerPtr(paramHolder,i,&output_int[i][0]);
+		 break;
+	  }
+   }
+
+   retCode = TA_CallFunc(paramHolder,0,size,&outBegIdx,&outNbElement);
+   if( retCode != TA_SUCCESS )
+   {
+      printf( "TA_CallFunc() failed zero data test [%d]\n", retCode );
+      TA_ParamHolderFree( paramHolder );
+      return TA_ABS_TST_FAIL_CALLFUNC_1;
+   }      
+
+   for( i=0; i < funcInfo->nbOutput; i++ )
+   {
+	  switch(outputInfo->type)
+	  {
+	  case TA_Output_Real:	     
+		for( j=0; j < outNbElement; j++ )
+		{
+			if( trio_isnan(output[i][j]) ||
+                trio_isinf(output[i][j]) )
+			{
+				printf( "Failed for output[%d][%d] = %e\n", i, j, output[i][j] );
+				return TA_ABS_TST_FAIL_INVALID_OUTPUT;
+			}
+		}
+		break;
+	  case TA_Output_Integer:	     
+		break;
+	  }
+   }
+
+   retCode = TA_ParamHolderFree( paramHolder );
+   if( retCode != TA_SUCCESS )
+   {
+      printf( "TA_ParamHolderFree failed [%d]\n", retCode );
+      return TA_ABS_TST_FAIL_PARAMHOLDERFREE;
+   }
+
+   return TA_TEST_PASS;
+}
+
+static ErrorNumber test_default_calls(void)
+{
+   ErrorNumber errNumber;
+   unsigned int i;
+   unsigned int sign;
+
+   errNumber = TA_TEST_PASS;
+
+   for( i=0; i < sizeof(inputNegData)/sizeof(double); i++ )
+   {
+      inputNegData[i] = -((double)((int)i));
+	  inputNegData_int[i] = -(int)i;
+   }
+
+   for( i=0; i < sizeof(inputZeroData)/sizeof(double); i++ )
+   {
+      inputZeroData[i] = 0.0;
+	  inputZeroData_int[i] = (int)inputZeroData[i];
+   }
+
+   for( i=0; i < sizeof(inputRandomData)/sizeof(double); i++ )
+   {
+      inputRandomData[i] = (double)rand()/97.234;
+	  inputRandomData_int[i] = (int)inputRandomData[i];
+   }
+
+   for( i=0; i < sizeof(inputRandFltEpsilon)/sizeof(double); i++ )
+   {
+       sign= (unsigned int)rand()%2;
+       inputRandFltEpsilon[i] = (sign?1.0:-1.0)*(FLT_EPSILON);
+       inputRandFltEpsilon_int[i] = sign?1:0;
+   }
+
+   for( i=0; i < sizeof(inputRandFltEpsilon)/sizeof(double); i++ )
+   {
+       sign= (unsigned int)rand()%2;
+       inputRandFltEpsilon[i] = (sign?1.0:-1.0)*(DBL_EPSILON);
+       inputRandFltEpsilon_int[i] = sign?1:-1;
+   }
+
+   TA_ForEachFunc( testDefault, &errNumber );
+   return errNumber;
+}
