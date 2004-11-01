bfd2d03f201e45f35a31a575573d39ea6e4073d1
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2004-11-01 04:16:51 +0000

    Add a lot of price bar validation.

--- src/ta_data/ta_history/ta_historycheck.c
+++ src/ta_data/ta_history/ta_historycheck.c
@@ -83,17 +83,23 @@ TA_RetCode TA_HistoryCheck( TA_Period           expectedPeriod,
                             unsigned int       *faultyField )
 {
    TA_PROLOG
-   unsigned int allFieldNull;
+   unsigned int i, defaultLoop;
+   TA_Timestamp *prevTimestamp;
+   TA_Timestamp *tempTimestamp;
+   TA_Real tempRealHigh, tempRealLow, tempRealOpen, tempRealClose;
 
    (void)faultyField;
    (void)faultyIndex;
    (void)fieldToCheck;
-   (void)expectedEnd;
-   (void)expectedStart;
 
-   TA_TRACE_BEGIN(  TA_HistoryCheckInternal );
+   TA_TRACE_BEGIN( TA_HistoryCheck );
 
    TA_ASSERT( history != NULL );
+
+   /***************************************************************
+    * First section detects issues that would indicate a flaw in  *
+    * the TA-Lib logic. For these, report a fatal internal error. *
+    ***************************************************************/
 
    /* Period shall be always set in the history. */
    if( history->period != expectedPeriod )
@@ -101,31 +107,34 @@ TA_RetCode TA_HistoryCheck( TA_Period           expectedPeriod,
       TA_TRACE_RETURN( TA_INTERNAL_ERROR(45) );
    }
 
-   /* Verify that an empty history is really empty. */
    if( (history->open  == NULL) &&
        (history->high  == NULL) &&
        (history->low   == NULL) &&
        (history->close == NULL) &&
        (history->volume == NULL) &&
        (history->openInterest == NULL) &&
-       (history->timestamp == NULL) )
+       (fieldToCheck != TA_TIMESTAMP) )
    {
-      allFieldNull = 1;
+      /* An empty history should have none of these array allocated. */
       if( history->nbBars != 0 )
       {
          TA_TRACE_RETURN( TA_INTERNAL_ERROR(46) );
-      }
+      }      
+      /* Nothing else to check for an empty history. */
+      TA_TRACE_RETURN( TA_SUCCESS );
    }
-   else
-      allFieldNull = 0;
-
-   if( (history->nbBars == 0) && !allFieldNull )
+   else if( history->nbBars == 0 )
    {
       TA_TRACE_RETURN( TA_INTERNAL_ERROR(47) );
    }
-
-   if( history->nbBars > 0 )
+   else if( history->nbBars < 0 )
    {
+      TA_TRACE_RETURN( TA_INTERNAL_ERROR(168) );
+   }
+   else if( history->nbBars > 0 )
+   {
+      TA_ASSERT( history->timestamp != NULL );
+
       /* Verify that the data is not older than "start". */
       if( expectedStart )
       {
@@ -157,8 +166,178 @@ TA_RetCode TA_HistoryCheck( TA_Period           expectedPeriod,
       }
    }
 
-   /* !!! Some more runtime verification could be added here... */
+   if( fieldToCheck != TA_ALL )
+   {
+      #define CHECK_EXPECTED_FIELD(lc,uc,rc) { \
+         if( ((history->lc) && !(fieldToCheck&TA_##uc)) || (!(history->lc) && (fieldToCheck&TA_##uc)) ) \
+         { \
+            TA_TRACE_RETURN( TA_INTERNAL_ERROR(rc) ); \
+         } \
+      }
+      CHECK_EXPECTED_FIELD( open,         OPEN,         169 );
+      CHECK_EXPECTED_FIELD( high,         HIGH,         170 );
+      CHECK_EXPECTED_FIELD( low ,         LOW,          171 );
+      CHECK_EXPECTED_FIELD( close,        CLOSE,        172 );
+      CHECK_EXPECTED_FIELD( volume,       VOLUME,       173 );
+      CHECK_EXPECTED_FIELD( openInterest, OPENINTEREST, 174 );
+      #undef CHECK_EXPECTED_FIELD
+   }
 
+   /***************************************************************
+    * Second section detects issues related to the data returned  *
+    * by the data source. These are reported as normal errors to  *
+    * the caller.                                                 *
+    ***************************************************************/
+
+   if( history->nbBars != 0 )
+   {
+     /* Optimized loop depending which fields are available
+      * 
+      *               Loop #
+      *            1  2  3  4  5
+      * Open       *  *  *         
+      * High       *  *  *         
+      * Low        *  *  *         
+      * Close      *  *  *  *  *   
+      * Volume     *  *     *   
+      * OI         *
+      *
+      * These are the expected most common field combinations, all 
+      * other combinations are handled by the "defaultLoop"
+      */
+      defaultLoop = 1;
+
+      #define CHECK_ASCENDING_TIMESTAMP { \
+         tempTimestamp = &history->timestamp[i]; \
+         if(TA_TimestampLess( tempTimestamp, prevTimestamp) ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_TIMESTAMP_ORDER ); \
+         prevTimestamp = tempTimestamp; \
+      }
+
+      #define CHECK_VOLUME { \
+         if( history->volume[i] < 0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_VOLUME_IS_NEGATIVE ); \
+      }
+
+      #define CHECK_OPEN_INTEREST { \
+         if( history->openInterest[i] < 0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_OI_IS_NEGATIVE ); \
+      }
+
+      #define CHECK_CLOSE_ONLY { \
+         if( history->close[i] < 0.0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_CLOSE_IS_NEGATIVE ); \
+      }
+
+      #define CHECK_OPEN_HIGH_LOW_CLOSE { \
+         tempRealHigh  = history->high[i]; \
+         tempRealLow   = history->low[i]; \
+         if( tempRealHigh < 0.0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_HIGH_IS_NEGATIVE ); \
+         if( tempRealLow < 0.0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_LOW_IS_NEGATIVE ); \
+         if( tempRealHigh < tempRealLow ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_LOW_EXCEED_HIGH ); \
+         tempRealOpen  = history->open[i]; \
+         if( tempRealOpen < 0.0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_OPEN_IS_NEGATIVE ); \
+         if( tempRealOpen > tempRealHigh ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_OPEN_EXCEED_HIGH ); \
+         if( tempRealOpen < tempRealLow ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_OPEN_BELOW_LOW ); \
+         tempRealClose = history->close[i]; \
+         if( tempRealClose < 0.0 ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_CLOSE_IS_NEGATIVE ); \
+         if( tempRealClose > tempRealHigh ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_CLOSE_EXCEED_HIGH ); \
+         if( tempRealClose < tempRealLow ) \
+            TA_TRACE_RETURN( TA_DATA_ERROR_CLOSE_BELOW_LOW ); \
+      }
+
+      if( history->close )
+      {
+         if( history->open && history->high && history->low )
+         {
+            if( history->volume )
+            {
+               if( history->openInterest )
+               {
+                  /* Loop #1 - Open,High,Low,Close,Volume,OpenInterest */
+                  defaultLoop = 0;
+                  prevTimestamp = &history->timestamp[0];
+                  for( i=1; i < history->nbBars; i++ )
+                  {
+                     CHECK_ASCENDING_TIMESTAMP;
+                     CHECK_VOLUME;
+                     CHECK_OPEN_INTEREST;
+                     CHECK_OPEN_HIGH_LOW_CLOSE;
+                  }
+               }
+               else
+               {
+                  /* Loop #2 - Open,High,Low,Close,Volume */
+                  defaultLoop = 0;
+                  prevTimestamp = &history->timestamp[0];
+                  for( i=1; i < history->nbBars; i++ )
+                  {
+                     CHECK_ASCENDING_TIMESTAMP;
+                     CHECK_VOLUME;
+                     CHECK_OPEN_HIGH_LOW_CLOSE;
+                  }
+               }
+            }
+            else if( !history->openInterest )
+            {
+               /* Loop #3 - Open,High,Low,Close */
+               defaultLoop = 0;
+               prevTimestamp = &history->timestamp[0];
+               for( i=1; i < history->nbBars; i++ )
+               {
+                  CHECK_ASCENDING_TIMESTAMP;
+                  CHECK_OPEN_HIGH_LOW_CLOSE;
+               }
+            }         
+         }
+         else if( (history->volume) && !(history->openInterest) )
+         {
+            /* Loop #4 - Close,Volume */
+            defaultLoop = 0;
+            prevTimestamp = &history->timestamp[0];
+            for( i=1; i < history->nbBars; i++ )
+            {
+               CHECK_ASCENDING_TIMESTAMP;
+               CHECK_CLOSE_ONLY;
+               CHECK_VOLUME;
+            }
+         }
+         else if( !(history->volume) && !(history->openInterest) )
+         {
+            /* Loop #5 - Close */
+            defaultLoop = 0;
+            prevTimestamp = &history->timestamp[0];
+            for( i=1; i < history->nbBars; i++ )
+            {
+               CHECK_ASCENDING_TIMESTAMP;
+               CHECK_CLOSE_ONLY;
+            }
+         }
+      }
+
+      if( defaultLoop )
+      {
+         prevTimestamp = &history->timestamp[0];
+         for( i=1; i < history->nbBars; i++ )
+         {
+            CHECK_ASCENDING_TIMESTAMP;
+            if( history->volume ) CHECK_VOLUME;
+            if( history->openInterest ) CHECK_OPEN_INTEREST;
+            if( history->close ) CHECK_CLOSE_ONLY;
+            /* !!! More test can be added here */
+         }
+      }      
+   }
+
+   /* All fine! */
    TA_TRACE_RETURN( TA_SUCCESS );
 }
 
