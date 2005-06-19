3201ba15e140b8201b2c8d3e02c3528305494759
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2005-06-19 00:10:17 +0000

    Change volume, openInterest data to integer.

--- src/tools/gen_data/mrg_gen.pl
+++ src/tools/gen_data/mrg_gen.pl
@@ -9,7 +9,7 @@
 # that, once merge together, they should form a pre-determine timeserie (pre-defined
 # price bars).
 #
-# If that pre-determine timeserie is not recognize by tha ta_selftest, we can assume
+# If that pre-determine timeserie is not recognize by tha ta_regtest, we can assume
 # that there is something broken in the merge logic.
 
 # This script needs the mrg.in file as input.
@@ -20,13 +20,13 @@
 # open goes from 2000.0001 to 2999.1000
 # close goes from 3000.0001 to 3999.1000
 # high goes from 4000.0001 to 4999.1000
-# volume goes from 5000 to 5999
-# openInterest goes from 6000 to 6999
+# volume goes from 10000 to 10999
+# openInterest goes from 20000 to  20999
 #
 # As you can see, each price in the price bar is going up in increment of 1.0001
-# With this scheme, no two price are identical.
+# With this scheme, no two prices are identical.
 #
-# The timestamp can start at any date, but must be in 1 day increment.
+# The timestamps can start at any date, but must be in 1 day increment.
 #
 
 $nbPriceBar = 1000;
@@ -35,8 +35,8 @@ $low    = $nbPriceBar + 0.0001;
 $open = $low + $nbPriceBar;
 $close = $open + $nbPriceBar;
 $high  = $close + $nbPriceBar;
-$volume = $high + $nbPriceBar;
-$openInterest = $volume + $nbPriceBar;
+$volume = 10000;
+$openInterest = 20000;
 
 # Determine starting date
 $nbSec = rand 1000000000;
@@ -54,8 +54,17 @@ while( $line = <in> )
 {
    # Build the valid price bar
    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime $nbSec;
-   @timestamp = ($mon,"-",$mday,"-",$year+1900);
-   @priceBar = (@timestamp,",",$open,",",$high,",",$low,",",$close,",",$volume,",",$openInterest,"\n");
+   $mon++;
+   $monStr = sprintf("%02d", $mon);
+   $mdayStr = sprintf("%02d", $mday);
+   @timestamp = ($monStr,"-",$mdayStr,"-",$year+1900);
+
+   $openStr  = sprintf("%.04f",$open);
+   $highStr  = sprintf("%.04f",$high);
+   $lowStr   = sprintf("%.04f",$low);
+   $closeStr = sprintf("%.04f",$close);
+   
+   @priceBar = (@timestamp,",",$openStr,",",$highStr,",",$lowStr,",",$closeStr,",",$volume,",",$openInterest,"\n");
 
    # Build the price bar with invalid prices (shall be override in the TA_LIB merge logic).
    @badPriceBar = (@timestamp,",1,2,3,4,5,6\n");
@@ -79,8 +88,9 @@ while( $line = <in> )
    $high += 1.0001;
    $low += 1.0001;
    $close += 1.0001;
-   $volume += 1.0001;
-   $openInterest += 1.0001;
+   
+   $volume += 1;
+   $openInterest += 1;
 
    $nbSec += (60*60*24);  
    $nbLine++;
