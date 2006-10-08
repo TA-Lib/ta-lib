3962eeb8dbd938997ead78808bc3d8e3f27e18fc
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2006-10-08 03:23:58 +0000

    Minor change to avoid duplicate declarations.

--- trunk/ta-lib/c/include/ta_defs.h
+++ trunk/ta-lib/c/include/ta_defs.h
@@ -43,57 +43,58 @@
  ***    INT_MIN : The minimal value for Int32
  ***    INT_MAX : The maximal value for Int32
  ***/
-#if defined( _MANAGED )
-  /* Int32, UInt32, Int64 and UInt64 are built-in for .NET */	
-  #define INT_MIN (Int32::MinValue)
-  #define INT_MAX (Int32::MaxValue)
-#elif defined( _JAVA )
-  #define INT_MIN Integer.MIN_VALUE
-  #define INT_MAX Integer.MAX_VALUE
-#else
-  #include <limits.h>
+#ifndef FD_DEFS_H
+  #if defined( _MANAGED )
+    /* Int32, UInt32, Int64 and UInt64 are built-in for .NET */	
+    #define INT_MIN (Int32::MinValue)
+    #define INT_MAX (Int32::MaxValue)
+  #elif defined( _JAVA )
+    #define INT_MIN Integer.MIN_VALUE
+    #define INT_MAX Integer.MAX_VALUE
+  #else
+    #include <limits.h>
 
-  /* Identify if 64 bits platform with __64BIT__.
-   * Can also be done from compiler command line. 
-   */
-  #if defined(_WIN64)
-     #define __64BIT__ 1
-  #endif
+    /* Identify if 64 bits platform with __64BIT__.
+     * Can also be done from compiler command line. 
+     */
+    #if defined(_WIN64)
+       #define __64BIT__ 1
+    #endif
 
-  #if defined( __LP64__ ) || defined( _LP64 )
-     #define __64BIT__ 1
-  #endif
+    #if defined( __LP64__ ) || defined( _LP64 )
+       #define __64BIT__ 1
+    #endif
 
-  /* Check also for some known GCC def for 64 bits platform. */
-  #if defined(__alpha__)\
-      ||defined(__ia64__)\
-      ||defined(__ppc64__)\
-      ||defined(__s390x__)\
-      ||defined(__x86_64__)
-     #define __64BIT__ 1
-  #endif		  
+    /* Check also for some known GCC def for 64 bits platform. */
+    #if defined(__alpha__)\
+        ||defined(__ia64__)\
+        ||defined(__ppc64__)\
+        ||defined(__s390x__)\
+        ||defined(__x86_64__)
+       #define __64BIT__ 1
+    #endif		  
 		   
-  #if !defined(__MACTYPES__)
-      typedef signed int   Int32;
-      typedef unsigned int UInt32;
+    #if !defined(__MACTYPES__)
+        typedef signed int   Int32;
+        typedef unsigned int UInt32;
 
-      #if defined(_WIN32) || defined(_WIN64)
-         /* See "Windows Data Types". Platform SDK. MSDN documentation. */
-         typedef signed __int64   Int64;
-         typedef unsigned __int64 UInt64;
-      #else
-         #if defined(__64BIT__)
-            /* Standard LP64 model for 64 bits Unix platform. */
-            typedef signed long   Int64;
-            typedef unsigned long UInt64;
-         #else
-            /* Standard ILP32 model for 32 bits Unix platform. */
-            typedef signed long long   Int64;
-            typedef unsigned long long UInt64;
+        #if defined(_WIN32) || defined(_WIN64)
+           /* See "Windows Data Types". Platform SDK. MSDN documentation. */
+           typedef signed __int64   Int64;
+           typedef unsigned __int64 UInt64;
+        #else
+           #if defined(__64BIT__)
+              /* Standard LP64 model for 64 bits Unix platform. */
+              typedef signed long   Int64;
+              typedef unsigned long UInt64;
+           #else
+              /* Standard ILP32 model for 32 bits Unix platform. */
+              typedef signed long long   Int64;
+              typedef unsigned long long UInt64;
+           #endif
          #endif
-       #endif
+    #endif
   #endif
-
 #endif
 
 /* Enumeration and macros to abstract from syntax difference 
