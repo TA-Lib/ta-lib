d47bba11f94826bed8912d82afcefb907ca4c496
robbymeier <robbymeier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2006-10-09 18:31:13 +0000

    1) Removed TA_OUT_THIN_LINE, TA_OUT_NORM_LINE, TA_OUT_THICK_LINE. 2) Added TA_OUT_PATTERN_BOOL, TA_OUT_PATTERN_STRENGTH, TA_OUT_POSITIVE, TA_OUT_NEGATIVE, TA_OUT_ZERO 3) Added function to replace reserved XML characters with their corresponding escape characters (otherwise we might end up with an invalid XML file). 4) Added ShortDescription and LongDescription tag to OptionalInputArgument tag. The former can be used for tooltips and the later provides more detailed information about an optional input argument. 5) Added Percent, Degree, Currency, Advanced flags for tag OptionalInputArgument.

--- include/ta_abstract.h
+++ include/ta_abstract.h
@@ -359,14 +359,17 @@ typedef int TA_OptInputFlags;
  * could be done with the output.
  */
 typedef int TA_OutputFlags;
-#define TA_OUT_LINE       0x00000001 /* Suggest to display as a connected line. */
-#define TA_OUT_DOT_LINE   0x00000002 /* Suggest to display as a 'dotted' line. */
-#define TA_OUT_DASH_LINE  0x00000004 /* Suggest to display as a 'dashed' line. */
-#define TA_OUT_DOT        0x00000008 /* Suggest to display with dots only. */
-#define TA_OUT_HISTO      0x00000010 /* Suggest to display as an histogram. */
-#define TA_OUT_THIN_LINE  0x00000100 /* Suggest an output line weight. */
-#define TA_OUT_NORM_LINE  0x00000200
-#define TA_OUT_THICK_LINE 0x00000400
+#define TA_OUT_LINE              0x00000001 /* Suggest to display as a connected line. */
+#define TA_OUT_DOT_LINE          0x00000002 /* Suggest to display as a 'dotted' line. */
+#define TA_OUT_DASH_LINE         0x00000004 /* Suggest to display as a 'dashed' line. */
+#define TA_OUT_DOT               0x00000008 /* Suggest to display with dots only. */
+#define TA_OUT_HISTO             0x00000010 /* Suggest to display as an histogram. */
+#define TA_OUT_PATTERN_BOOL      0x00000020 /* Indicates if pattern exists (!=0) or not (0) */
+#define TA_OUT_PATTERN_BULL_BEAR 0x00000040 /* =0 no pattern, > 0 bullish, < 0 bearish */
+#define TA_OUT_PATTERN_STRENGTH  0x00000080 /* =0 neutral, ]0..100] getting bullish, ]100..200] bullish, [-100..0[ getting bearish, [-200..100[ bearish */
+#define TA_OUT_POSITIVE          0x00000100 /* Output can be positive */
+#define TA_OUT_NEGATIVE          0x00000200 /* Output can be negative */
+#define TA_OUT_ZERO              0x00000400 /* Output can be zero */
 
 
 /* The following 3 structures will exist for each input, optional
