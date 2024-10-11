1ff20f3cb600adaa89c5eac31a05585d0fa88f76
Halo <xv2017@outlook.com>
2024-10-11 18:48:22 +0800

    add macOS libtool support.
    the libtool named glibtoolize on macOS.

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,13 @@ echo aclocal
 aclocal || exit
 echo autoheader
 autoheader || exit
-echo libtoolize --copy --force
-libtoolize --copy --force || exit
+if [[ `uname -o` -eq 'Darwin' ]]; then
+    echo glibtoolize --copy --force
+    glibtoolize --copy --force || exit
+else
+    echo libtoolize --copy --force
+    libtoolize --copy --force || exit
+fi
 echo automake -a -c
 automake -a -c || exit
 echo autoconf
