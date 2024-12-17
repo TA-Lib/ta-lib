846f9383fba69fcf287ee0e9801b95900599206b
mario4tier <mario4tier@users.noreply.github.com>
2024-12-17 18:24:04 -0500

    Modernize makefile to compile with at least C11

--- include/ta_common.h
+++ include/ta_common.h
@@ -151,7 +151,7 @@ TA_RetCode TA_Shutdown( void );
  * This value is updated whenever a make, cmake or any source files
  * modification should trig a repackaging of TA-Lib.
  */
-#define TA_LIB_SOURCES_DIGEST e8a28683cedbbd1298a77453ba5c7d63
+#define TA_LIB_SOURCES_DIGEST 187bc60f9b0a5ab37373c6ba6112ca82
 
 #ifdef __cplusplus
 }
