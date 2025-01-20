85e543b35128992a3f0eca5c9c1774fbcb1467a3
mario4tier <mario4tier@users.noreply.github.com>
2025-01-20 13:31:57 -0500

    Update copyright year (2025)

--- src/ta_func/ta_APO.c
+++ src/ta_func/ta_APO.c
@@ -1,4 +1,4 @@
-/* TA-LIB Copyright (c) 1999-2024, Mario Fortier
+/* TA-LIB Copyright (c) 1999-2025, Mario Fortier
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or
