10fd680f56eaee706f9e56b02d49f91f9f9c5669
CaptainTrunky <captaintrunky@gmail.com>
2026-06-05 14:58:25 +0200

    Add basic Conan support

---
+++ conanfile.py
@@ -0,0 +1,73 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import copy
+import os
+
+
+class TaLibConan(ConanFile):
+    name = "ta-lib"
+    version = "0.6.4"
+
+    license = "BSD-3-Clause"
+    url = "https://github.com/ta-lib/ta-lib"
+    description = "TA-Lib provides common functions for the technical analysis of stock/future/commodity market data."
+    topics = ("technical-analysis", "finance", "trading")
+
+    package_type = "library"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    exports_sources = "CMakeLists.txt", "cmake/*", "include/*", "src/*", "LICENSE", "README.md"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_DEV_TOOLS"] = False
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+
+    def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "ta-lib")
+        self.cpp_info.set_property("cmake_target_name", "ta-lib")
+        self.cpp_info.set_property("pkg_config_name", "ta-lib")
+
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs.append("m")
+
+        if self.options.shared:
+            self.cpp_info.libs = ["ta-lib"]
+        else:
+            if self.settings.os == "Windows":
+                self.cpp_info.libs = ["ta-lib-static"]
+                self.cpp_info.defines.append("TA_LIB_STATIC")
+            else:
+                self.cpp_info.libs = ["ta-lib"]
+
+        if self.settings.os != "Windows":
+            self.cpp_info.includedirs = ["include/ta-lib"]
