d7b8a5a9196ca5670a697fd21137c687479d3257
mario4tier <mario4tier@users.noreply.github.com>
2026-08-12 21:47:44 -0400

    chore(codegen): regenerate after merging dev (WAD gains OpenAndFillInternal)

--- src/ta_func/ta_WAD.c
+++ src/ta_func/ta_WAD.c
@@ -389,6 +389,12 @@ TA_LIB_API TA_RetCode TA_WAD_OpenAndFill( TA_WAD_Stream **stream, const double i
    return TA_WAD_OpenCore( stream, inHigh, inLow, inClose, 0, historyLen, outBegIdx, outNBElement, outReal, 1 );
 }
 
+/* Private function, not in public API. */
+TA_RetCode TA_WAD_OpenAndFillInternal( struct TA_WAD_Stream **stream, const double inHigh[], const double inLow[], const double inClose[], int startIdx, int historyLen, int *outBegIdx, int *outNBElement, double outReal[] )
+{
+   return TA_WAD_OpenCore( stream, inHigh, inLow, inClose, startIdx, historyLen, outBegIdx, outNBElement, outReal, 1 );
+}
+
 TA_LIB_API TA_RetCode TA_WAD_Update( TA_WAD_Stream *stream, double inHigh, double inLow, double inClose, double *outReal )
 {
    if( !stream || !outReal ) return TA_BAD_PARAM;
